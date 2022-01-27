8db3e797510fdb453b8c3bdc3a7a92ef6a2c380d
admin <2780288581@qq.com>
2022-01-27 22:12:22 +0800

    静态页面

--- doublekit-pipeline-ui/src/modules/pipeline/pipeline.js
+++ doublekit-pipeline-ui/src/modules/pipeline/pipeline.js
@@ -1,4 +1,4 @@
-import React  from "react";
+import React,{Component}  from "react";
 import PipelineHeader from "./components/pipelineHeader";
 import PipelineMy from "./pages/my/pipelineMy";
 import PipelineAll from "./pages/all/pipelineAll";
@@ -7,27 +7,29 @@ import {withRouter} from "react-router-dom";
 import {PlusOutlined} from "@ant-design/icons";
 
 const { TabPane } = Tabs;
-const Pipeline=()=>{
-    return(
-        <div className='pipeline'>
-            <div className='pipeline-top'>
-                <PipelineHeader/>
+class Pipeline extends Component{
+    render(){
+        return(
+            <div className='pipeline'>
+                <div className='pipeline-top'>
+                    <PipelineHeader/>
+                </div>
+                <Tabs type="card">
+                    <TabPane tab="所有视图" key="1">
+                        <PipelineAll/>
+                    </TabPane>
+                    <TabPane tab="我的" key="2">
+                        <PipelineMy/>
+                    </TabPane>
+                </Tabs>
+                <div className='pipeline-add'>
+                    <Button onClick={()=>this.props.history.push('/system')}>
+                        <PlusOutlined/>
+                    </Button>
+                </div>
             </div>
-            <Tabs type="card">
-                <TabPane tab="所有视图" key="1">
-                    <PipelineAll/>
-                </TabPane>
-                <TabPane tab="我的" key="2">
-                    <PipelineMy/>
-                </TabPane>
-            </Tabs>
-            <div className='pipeline-add'>
-                <Button onClick={()=>this.props.history.push('/system')}>
-                    <PlusOutlined/>
-                </Button>
-            </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default withRouter(Pipeline)
