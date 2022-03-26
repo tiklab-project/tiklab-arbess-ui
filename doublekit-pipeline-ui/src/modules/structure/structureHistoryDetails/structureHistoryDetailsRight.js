8e179f9fa58601cb49a48c0800dfe57bc4314648
admin <2780288581@qq.com>
2022-03-26 18:56:46 +0800

    2

---
+++ doublekit-pipeline-ui/src/modules/structure/structureHistoryDetails/structureHistoryDetailsRight.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const StructureHistoryDetailsRight = props =>{
+    return(
+        <div className='structureHistory-details-right'>
+            structureHistory-details-right
+        </div>
+    )
+}
+
+export default StructureHistoryDetailsRight
